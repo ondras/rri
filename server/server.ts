173e8e7601c47e21d3f53a1e283240aef9428ada
Ondřej Žára <ondrej.zara@gmail.com>
2023-02-26 16:37:41 +0100

    better logging

--- server/server.ts
+++ server/server.ts
@@ -2,18 +2,18 @@ import { serve } from "https://deno.land/std@0.178.0/http/server.ts";
 import Player from "./player.ts";
 
 
-async function handleReq(req: Request) {
+async function handleReq(req: Request, conn: ConnInfo) {
+	console.log("new http request", conn.remoteAddr, req.url);
 	try {
 		const { socket, response } = Deno.upgradeWebSocket(req);
-		console.log("new websocket connection");
+		console.log("accepted websocket upgrade")
 		new Player(socket);
 		return response;
 	} catch {
-		console.error("failed to accept websocket for url", req.url);
+		console.error("failed to accept websocket, redirecting away");
 		return Response.redirect("https://ondras.github.io/rri/");
 	}
 }
 
 const port = Number(Deno.args[0] || "80");
 serve(handleReq, {port})
-console.log("http server is running on", port);
